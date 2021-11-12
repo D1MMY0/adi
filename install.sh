71d7f3c9d15b75bbe1d0869454f50368ca253221
D1MMY <76232394+D1MMY0@users.noreply.github.com>
2021-11-12 20:41:17 +0530

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+#CODE_BY_D1MY
+clear
+sleep 1
+echo ""
+echo ""
+echo " 
+████████╗███████╗░█████╗░██╗░░██╗
+╚══██╔══╝██╔════╝██╔══██╗██║░░██║
+░░░██║░░░█████╗░░██║░░╚═╝███████║
+░░░██║░░░██╔══╝░░██║░░██╗██╔══██║
+░░░██║░░░███████╗╚█████╔╝██║░░██║
+░░░╚═╝░░░╚══════╝░╚════╝░╚═╝░░╚═╝ " | lolcat
+
+
+
+
+echo ""
+sleep 1
+clear
+
+pkg install php -y
+pkg install openssh -y
+pkg install unzip -y
+pkg install curl -y
+Pkg install wget -y
+pkg install python -y
+pip install lolcat
+sleep 1
+clear
+
+echo "Loading==> " | lolcat
+sleep 1
+clear
+echo "Loading====> " | lolcat
+sleep 1
+clear
+echo "Loading=======> " | lolcat
+sleep 1
+clear
+echo " done " | lolcat
+sleep 1
+clear
+unzip pro.zip
+clear
+rm -rf pro.zip
+clear
+chmod +x carder.sh
+sleep 1
+
+bash carder.sh
+
+
+
+
+
