efed289d3b75a7ff6445bb182111109459c4ae1a
D1MMY <76232394+D1MMY0@users.noreply.github.com>
2022-02-04 15:25:19 +0530

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+#𝐂𝐎𝐃𝐄 𝐁𝐘 𝐃𝐈𝐌𝐌𝐘
+clear
+sleep 1
+echo ""
+echo ""
+echo " 
+████████╗███████╗░█████╗░██╗░░██╗
+╚══██╔══╝██╔════╝██╔══██╗██║░░██║
+░░░██║░░░█████╗░░██║░░╚═╝███████║
+░░░██║░░░██╔══╝░░██║░░██╗██╔══██║
+░░░██║░░░███████╗╚█████╔╝██║░░██║
+░░░╚═╝░░░╚══════╝░╚════╝░╚═╝░░╚═╝ " | lolcat
+
+
+
+
+echo ""
+sleep 2
+clear
+pkg install python2
+pkg install espeak
+pkg install php -y
+pkg install openssh -y
+pkg install unzip -y
+pkg install curl -y
+Pkg install wget -y
+pkg install python -y
+pip install lolcat
+sleep 1
+clear
+
+echo "Loading==> " | lolcat
+sleep 2
+clear
+echo "Loading====> " | lolcat
+sleep 1
+clear
+echo "Loading=======> " | lolcat
+sleep 2
+clear
+echo " done " | lolcat
+sleep 1
+clear
+unzip pro.zip
+clear
+rm -rf pro.zip
+clear
+chmod +x start
+sleep 1
+
+bash start
+
+
+
+
+
